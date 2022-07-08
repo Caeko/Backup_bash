a343d79161ea723045f2c6611e04ec55c0b250e0
Caeko <79210371+Caeko@users.noreply.github.com>
2022-07-08 16:47:28 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "------------------------------------------------------------------"
+echo "Hello and Welcome on the backup script :) !"
+echo "Write by Caeko"
+echo "------------------------------------------------------------------"
+
+date=$(date +%Y-%m-%d)
+file_backup=PathOfYourFileYouWantBackup/
+dest=DestinationPathYouWantToHaveYourBackup/
+filename=back-${file_backup//\//_}-${date}.tar.gz
+
+if [ -f ${filename} ]; then
+	echo "${filename} already exists !"
+else
+  tar -czvf ${dest}/${filename} ${file_backup}
+fi
